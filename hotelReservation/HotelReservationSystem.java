f1282cc98f4987329a29319e0817949819b8cb78
tejaswini_14 <shelaketejaswini14@gmail.com>
2024-09-04 15:55:45 +0530

    Added: Welcome message

---
+++ hotelReservation/HotelReservationSystem.java
@@ -0,0 +1,10 @@
+package com.hotelReservation;
+
+public class HotelReservationSystem {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to hotel reservation system");
+	}
+
+}
